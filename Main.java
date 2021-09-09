0a9fada71557c05a5d23c0f9239c4b505493410d
Vlad1921 <77584644+Vlad1921@users.noreply.github.com>
2021-09-09 18:31:59 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        int[] num = new int[2];
+
+        num[0] = 20;
+
+        num[1] = 55;
+
+        for (int i = 0; i < num.length; i++) {
+
+            num[i] = (int) (num[i] * 1.1);
+
+            System.out.println(num[i]);
+        }
+    }
+}
